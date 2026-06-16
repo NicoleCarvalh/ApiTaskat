efcc3d463c37b889800bd64a64e2d82162502a47
NicoleCarvalh <nicolecarvalho6@gmail.com>
2026-06-16 09:18:11 -0300

    fix: alteração de LocalDate para LocalDateTime e limpeza de código comentados (getters e setters criados sem lombok)

--- src/main/java/com/Taskat/model/Tarefa.java
+++ src/main/java/com/Taskat/model/Tarefa.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 
@@ -36,68 +37,8 @@ public class Tarefa {
     @Enumerated(EnumType.STRING)
     private Status status = Status.PENDENTE;
 
-    private LocalDate dtVencimento;
+    private LocalDateTime dtVencimento;
 
-    private LocalDate dtConclusao;
-
-//    public Tarefa() {
-//    }
-//
-//    public Tarefa(Long id, String titulo, String descricao, Status status, LocalDate dtVencimento, LocalDate dtConclusao) {
-//        this.id = id;
-//        this.titulo = titulo;
-//        this.descricao = descricao;
-//        this.status = status;
-//        this.dtVencimento = dtVencimento;
-//        this.dtConclusao = dtConclusao;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getTitulo() {
-//        return titulo;
-//    }
-//
-//    public void setTitulo(String titulo) {
-//        this.titulo = titulo;
-//    }
-//
-//    public String getDescricao() {
-//        return descricao;
-//    }
-//
-//    public void setDescricao(String descricao) {
-//        this.descricao = descricao;
-//    }
-//
-//    public Status getStatus() {
-//        return status;
-//    }
-//
-//    public void setStatus(Status status) {
-//        this.status = status;
-//    }
-//
-//    public LocalDate getDtVencimento() {
-//        return dtVencimento;
-//    }
-//
-//    public void setDtVencimento(LocalDate dtVencimento) {
-//        this.dtVencimento = dtVencimento;
-//    }
-//
-//    public LocalDate getDtConclusao() {
-//        return dtConclusao;
-//    }
-//
-//    public void setDtConclusao(LocalDate dtConclusao) {
-//        this.dtConclusao = dtConclusao;
-//    }
+    private LocalDateTime dtConclusao;
 }
 
